84d034f10e83d5b91a11af4f8ace9768ee790d89
andih <andi.hofstadler@gmx.at>
2020-06-18 21:10:54 +0200

    structure

--- vevw/src/main/java/com/luftenberg/vevw/model/SomeEntity.java
+++ vevw/src/main/java/com/luftenberg/vevw/model/SomeEntity.java
@@ -2,9 +2,6 @@ package com.luftenberg.vevw.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.springframework.lang.NonNull;
--- vevw/src/test/java/com/luftenberg/vevw/Service/SomeEntityServiceImplTest.java
+++ vevw/src/test/java/com/luftenberg/vevw/Service/SomeEntityServiceImplTest.java
@@ -1,15 +1,8 @@
 package com.luftenberg.vevw.Service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.luftenberg.vevw.VereinsVerwaltungApplication;
 import com.luftenberg.vevw.VereinsVerwaltungApplicationTests;
-import com.luftenberg.vevw.model.SomeEntity;
 
 
 class SomeEntityServiceImplTest extends VereinsVerwaltungApplicationTests {
